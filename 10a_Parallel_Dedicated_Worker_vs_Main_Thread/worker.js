00dec7f26ab6dbb314ea1b147608f9b429932a8a
AlexinD <dalexin@codemastersintl.com>
2025-03-10 00:50:24 +0400

    Add initial diagrams and demos for event loop concepts
    This commit introduces draw.io diagrams visualizing the render and event loop processes. It also includes HTML demos to illustrate blocking operations and setTimeout clamping, providing interactive examples for understanding key browser performance behaviors.

--- 10a_Parallel_Dedicated_Worker_vs_Main_Thread/worker.js
+++
@@ -1,8 +0,0 @@
-onmessage = (event) => {
-    console.log("Worker received message:", event.data);
-
-    let start = performance.now();
-    while (performance.now() - start < 5000) {}
-
-    postMessage("Heavy task done in Worker!");
-};
